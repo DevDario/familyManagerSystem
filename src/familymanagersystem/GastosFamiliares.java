f0fa0d056555565292d524d6a8235148eaa7f96e
Dário Silva <dariosilva13222@gmail.com>
2023-03-05 00:32:38 +0000

    [feat💡]:associated the 'Conta' class
    I've associated the 'Conta' class to this class and alson pudated the project colaborators names.

--- src/familymanagersystem/GastosFamiliares.java
+++ src/familymanagersystem/GastosFamiliares.java
@@ -2,20 +2,61 @@ package familymanagersystem;
 
 /**
  *
- * @author Dário Silva
+ * @author Dário Silva & Délcio Morais
  * @since 2023
  * @version 1.0
  */
 public class GastosFamiliares extends Familia {
-    protected float value;
+    protected Double value;
     protected String item;
     protected String date;
+    Conta contaFamilia;
 
-    public float getValue() {
+    public Conta getContaFamilia() {
+        return contaFamilia;
+    }
+
+    public void setContaFamilia(Conta contaFamilia) {
+        this.contaFamilia = contaFamilia;
+    }
+
+    public static int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static void setPhoneNumber(int phoneNumber) {
+        Familia.phoneNumber = phoneNumber;
+    }
+
+    public static int getFamilyId() {
+        return familyId;
+    }
+
+    public static void setFamilyId(int familyId) {
+        Familia.familyId = familyId;
+    }
+
+    public static int getNumberOfMembers() {
+        return numberOfMembers;
+    }
+
+    public static void setNumberOfMembers(int numberOfMembers) {
+        Familia.numberOfMembers = numberOfMembers;
+    }
+
+    public static String getFamilyLastname() {
+        return familyLastname;
+    }
+
+    public static void setFamilyLastname(String familyLastname) {
+        Familia.familyLastname = familyLastname;
+    }
+
+    public Double getValue() {
         return value;
     }
 
-    public void setValue(float value) {
+    public void setValue(Double value) {
         this.value = value;
     }
 
