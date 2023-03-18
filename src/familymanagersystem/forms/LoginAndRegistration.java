806cb8aef23db9a94d261332dc91fbf52f6cb301
Dário Silva <dariosilva13222@gmail.com>
2023-03-18 14:28:56 +0000

    [feat💡]: calls the `RegistratioMainScreen` when clicked.

--- src/familymanagersystem/forms/LoginAndRegistration.java
+++ src/familymanagersystem/forms/LoginAndRegistration.java
@@ -39,7 +39,7 @@ public class LoginAndRegistration extends javax.swing.JFrame {
         jPasswordLabel = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jPasswordField = new javax.swing.JPasswordField();
-        jLabel2 = new javax.swing.JLabel();
+        jCreateFamilyButton = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Sign up | Sign in");
@@ -85,10 +85,15 @@ public class LoginAndRegistration extends javax.swing.JFrame {
         jPasswordField.setForeground(new java.awt.Color(255, 255, 255));
         jPasswordField.setBorder(javax.swing.BorderFactory.createMatteBorder(2, 2, 2, 2, new java.awt.Color(37, 37, 37)));
 
-        jLabel2.setFont(new java.awt.Font("Poppins Light", 0, 13)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel2.setText("create family");
-        jLabel2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jCreateFamilyButton.setFont(new java.awt.Font("Poppins Light", 0, 13)); // NOI18N
+        jCreateFamilyButton.setForeground(new java.awt.Color(255, 255, 255));
+        jCreateFamilyButton.setText("create family");
+        jCreateFamilyButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jCreateFamilyButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jCreateFamilyButtonMouseClicked(evt);
+            }
+        });
 
         javax.swing.GroupLayout jBackgroundLayout = new javax.swing.GroupLayout(jBackground);
         jBackground.setLayout(jBackgroundLayout);
@@ -109,7 +114,7 @@ public class LoginAndRegistration extends javax.swing.JFrame {
                             .addComponent(jPasswordField)))
                     .addGroup(jBackgroundLayout.createSequentialGroup()
                         .addGap(146, 146, 146)
-                        .addComponent(jLabel2)))
+                        .addComponent(jCreateFamilyButton)))
                 .addContainerGap(39, Short.MAX_VALUE))
         );
         jBackgroundLayout.setVerticalGroup(
@@ -128,7 +133,7 @@ public class LoginAndRegistration extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 54, Short.MAX_VALUE)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(13, 13, 13)
-                .addComponent(jLabel2)
+                .addComponent(jCreateFamilyButton)
                 .addContainerGap())
         );
 
@@ -160,38 +165,46 @@ public class LoginAndRegistration extends javax.swing.JFrame {
             if (lastname.isBlank() || familyPassword.isBlank()) {
                 JOptionPane.showMessageDialog(null, "You Need To Fill All The Fields", "Please, Fill Out All Fields", 0);
             } else {
-                //checks if there's a family with the given lastname
-                File familyFolder = new File(defaultFamilyFilePath + lastname);
+                if (familyPassword.length() == 8) {
+                    //checks if there's a family with the given lastname
+                    File familyFolder = new File(defaultFamilyFilePath + lastname);
 
-                if (familyFolder.exists()) {
-                    //checks if there's a password file inside the directory
-                    File familyPasswordFile = new File(defaultFamilyFilePath + lastname + "\\familyPassword.txt");
+                    if (familyFolder.exists()) {
+                        //checks if there's a password file inside the directory
+                        File familyPasswordFile = new File(defaultFamilyFilePath + lastname + "\\familyPassword.txt");
 
-                    if (familyPasswordFile.exists()) {
-                        //reads the content(password) inside the file
-                        Scanner passwordReader = new Scanner(defaultFamilyFilePath + lastname + "\\familyPassword.txt");
+                        if (familyPasswordFile.exists()) {
+                            //reads the content(password) inside the file
+                            Scanner passwordReader = new Scanner(defaultFamilyFilePath + lastname + "\\familyPassword.txt");
 
-                        while (passwordReader.hasNextLine()) {
-                            String fileData = passwordReader.nextLine();
-                            //compares data
-                            if(fileData.equals(familyPassword)){
-                                //enters the system
-                                dispose();
-                                new FamilyManager().setVisible(true);
-                            }
-                            
-                        }//else
-                        JOptionPane.showMessageDialog(null, "Data provided isn't correct", "Password or Lastname Wrong", 3);
+                            while (passwordReader.hasNextLine()) {
+                                String fileData = passwordReader.nextLine();
+                                //compares data
+                                if (fileData.equals(familyPassword)) {
+                                    //enters the system
+                                    dispose();
+                                    new FamilyManager().setVisible(true);
+                                }
+
+                            }//else
+                            JOptionPane.showMessageDialog(null, "Data provided isn't correct", "Password or Lastname Wrong", 3);
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Family" + lastname + " it's irregular !", "Error: Irregular family found", 0);
+                        }
+
                     } else {
-                        JOptionPane.showMessageDialog(null, "Family" + lastname + " it's irregular !", "Error: Irregular family found", 0);
+                        JOptionPane.showMessageDialog(null, "Family " + lastname + " does not exists !", "Error 404: Family NOT found", 0);
                     }
-
-                } else {
-                    JOptionPane.showMessageDialog(null, "Family" + lastname + " does not exists !", "Error 404: Family NOT found", 0);
                 }
             }
         }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jCreateFamilyButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jCreateFamilyButtonMouseClicked
+        //opens Registration Main Screen
+        dispose();
+        new RegistrationMainScreen().setVisible(true);
+    }//GEN-LAST:event_jCreateFamilyButtonMouseClicked
 
     /**
      * @param args the command line arguments
@@ -231,8 +244,8 @@ public class LoginAndRegistration extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel jBackground;
     private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jCreateFamilyButton;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JTextField jLastnameField;
     private javax.swing.JLabel jLastnameLabel;
     private javax.swing.JPasswordField jPasswordField;
