cfa46dfaf519cdc5d4d7811ff228df4edfa4db1f
Dário Silva <dariosilva13222@gmail.com>
2023-03-19 22:01:36 +0000

    [style💅🏻]:resized the form.

--- src/familymanagersystem/forms/FamilyManager.java
+++ src/familymanagersystem/forms/FamilyManager.java
@@ -52,18 +52,12 @@ public class FamilyManager extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jBackground = new javax.swing.JPanel();
         jSystemLogo = new javax.swing.JLabel();
-        jRegistratePanel = new javax.swing.JPanel();
-        jfamilyIcon = new javax.swing.JLabel();
-        jRegisterFamilyTitle = new javax.swing.JLabel();
         jRegistratePanel1 = new javax.swing.JPanel();
         jRegisterCompanyTitle1 = new javax.swing.JLabel();
         jCompanyIcon = new javax.swing.JLabel();
         jRegistratePanel2 = new javax.swing.JPanel();
         jAdminIcon = new javax.swing.JLabel();
         jAdminTitle = new javax.swing.JLabel();
-        jEditDataPanel = new javax.swing.JPanel();
-        jfamilyIcon1 = new javax.swing.JLabel();
-        jRegisterFamilyTitle1 = new javax.swing.JLabel();
         goToLoginButton = new javax.swing.JLabel();
         goToLoginText = new javax.swing.JLabel();
 
@@ -88,44 +82,6 @@ public class FamilyManager extends javax.swing.JFrame {
         jBackground.setBackground(new java.awt.Color(19, 19, 19));
 
         jSystemLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/system_logo.png"))); // NOI18N
-
-        jRegistratePanel.setBackground(new java.awt.Color(45, 45, 45));
-        jRegistratePanel.setBorder(javax.swing.BorderFactory.createMatteBorder(4, 0, 0, 0, new java.awt.Color(110, 86, 219)));
-        jRegistratePanel.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jRegistratePanel.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jRegistratePanelMouseClicked(evt);
-            }
-        });
-
-        jfamilyIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/family_purple_icon.png"))); // NOI18N
-
-        jRegisterFamilyTitle.setFont(new java.awt.Font("Poppins Light", 0, 14)); // NOI18N
-        jRegisterFamilyTitle.setForeground(new java.awt.Color(255, 255, 255));
-        jRegisterFamilyTitle.setText("Register a Family");
-
-        javax.swing.GroupLayout jRegistratePanelLayout = new javax.swing.GroupLayout(jRegistratePanel);
-        jRegistratePanel.setLayout(jRegistratePanelLayout);
-        jRegistratePanelLayout.setHorizontalGroup(
-            jRegistratePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jRegistratePanelLayout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jfamilyIcon, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(58, 58, 58))
-            .addGroup(jRegistratePanelLayout.createSequentialGroup()
-                .addGap(35, 35, 35)
-                .addComponent(jRegisterFamilyTitle)
-                .addContainerGap(38, Short.MAX_VALUE))
-        );
-        jRegistratePanelLayout.setVerticalGroup(
-            jRegistratePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jRegistratePanelLayout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addComponent(jfamilyIcon)
-                .addGap(18, 18, 18)
-                .addComponent(jRegisterFamilyTitle)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
 
         jRegistratePanel1.setBackground(new java.awt.Color(45, 45, 45));
         jRegistratePanel1.setBorder(javax.swing.BorderFactory.createMatteBorder(4, 0, 0, 0, new java.awt.Color(110, 86, 219)));
@@ -192,49 +148,11 @@ public class FamilyManager extends javax.swing.JFrame {
         jRegistratePanel2Layout.setVerticalGroup(
             jRegistratePanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jRegistratePanel2Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(25, Short.MAX_VALUE)
                 .addComponent(jAdminIcon)
                 .addGap(18, 18, 18)
                 .addComponent(jAdminTitle)
                 .addContainerGap())
-        );
-
-        jEditDataPanel.setBackground(new java.awt.Color(45, 45, 45));
-        jEditDataPanel.setBorder(javax.swing.BorderFactory.createMatteBorder(4, 0, 0, 0, new java.awt.Color(110, 86, 219)));
-        jEditDataPanel.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jEditDataPanel.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jEditDataPanelMouseClicked(evt);
-            }
-        });
-
-        jfamilyIcon1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/edit_family_icon.png"))); // NOI18N
-
-        jRegisterFamilyTitle1.setFont(new java.awt.Font("Poppins Light", 0, 14)); // NOI18N
-        jRegisterFamilyTitle1.setForeground(new java.awt.Color(255, 255, 255));
-        jRegisterFamilyTitle1.setText("Edit Family Data");
-
-        javax.swing.GroupLayout jEditDataPanelLayout = new javax.swing.GroupLayout(jEditDataPanel);
-        jEditDataPanel.setLayout(jEditDataPanelLayout);
-        jEditDataPanelLayout.setHorizontalGroup(
-            jEditDataPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jEditDataPanelLayout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jfamilyIcon1, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(58, 58, 58))
-            .addGroup(jEditDataPanelLayout.createSequentialGroup()
-                .addGap(35, 35, 35)
-                .addComponent(jRegisterFamilyTitle1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        jEditDataPanelLayout.setVerticalGroup(
-            jEditDataPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jEditDataPanelLayout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addComponent(jfamilyIcon1)
-                .addGap(18, 18, 18)
-                .addComponent(jRegisterFamilyTitle1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         goToLoginButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/exit_icon.png"))); // NOI18N
@@ -256,46 +174,35 @@ public class FamilyManager extends javax.swing.JFrame {
             .addGroup(jBackgroundLayout.createSequentialGroup()
                 .addGroup(jBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jBackgroundLayout.createSequentialGroup()
-                        .addGap(89, 89, 89)
-                        .addGroup(jBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jEditDataPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jRegistratePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addGap(18, 18, 18)
+                        .addGap(110, 110, 110)
                         .addComponent(jRegistratePanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
+                        .addGap(101, 101, 101)
                         .addComponent(jRegistratePanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jBackgroundLayout.createSequentialGroup()
-                        .addGap(333, 333, 333)
+                        .addGap(308, 308, 308)
                         .addComponent(jSystemLogo)))
-                .addContainerGap(88, Short.MAX_VALUE))
+                .addContainerGap(188, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jBackgroundLayout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
                 .addComponent(goToLoginButton)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(goToLoginText)
-                .addGap(20, 20, 20))
+                .addGap(124, 124, 124))
         );
         jBackgroundLayout.setVerticalGroup(
             jBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jBackgroundLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jSystemLogo)
-                .addGap(46, 46, 46)
+                .addGap(57, 57, 57)
+                .addGroup(jBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jRegistratePanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jRegistratePanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 148, Short.MAX_VALUE)
                 .addGroup(jBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jBackgroundLayout.createSequentialGroup()
-                        .addComponent(jRegistratePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jEditDataPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(100, 100, 100))
-                    .addGroup(jBackgroundLayout.createSequentialGroup()
-                        .addGroup(jBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jRegistratePanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jRegistratePanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(jBackgroundLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(goToLoginButton)
-                            .addComponent(goToLoginText))
-                        .addGap(20, 20, 20))))
+                    .addComponent(goToLoginButton)
+                    .addComponent(goToLoginText))
+                .addGap(50, 50, 50))
         );
 
         getContentPane().add(jBackground, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 810, 520));
@@ -317,14 +224,6 @@ public class FamilyManager extends javax.swing.JFrame {
         xMouse = evt.getX();
         yMouse = evt.getY();
     }//GEN-LAST:event_formMousePressed
-
-    private void jRegistratePanelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jRegistratePanelMouseClicked
-        //closes the current window
-        dispose();
-
-        //opens the registrationMainScreen form
-        new RegistrationMainScreen().setVisible(true);
-    }//GEN-LAST:event_jRegistratePanelMouseClicked
 
     private void jRegistratePanel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jRegistratePanel2MouseClicked
         //closes the current window
@@ -504,84 +403,6 @@ public class FamilyManager extends javax.swing.JFrame {
         }
     }
 
-    private void jEditDataPanelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jEditDataPanelMouseClicked
-        // opens a jDialog asking to the user wich option would he like to edit
-        String userEditOption = JOptionPane.showInputDialog(null, "<html>Wich item would you like to edit ?<br><ol><li>Edit Family's info</li><li>Edit a Father data</li><li>Edit a Mother Data</li><li>Edit a son's data</li></ol></html>", "Choose a option", 2);
-
-        switch (userEditOption) {
-            case "1":
-                //edits all info related to the family
-                String familyEditOption = JOptionPane.showInputDialog(null, "<html>Wich family data would you like to edit ?<br><ol><li>Edit Last name</li><li>Edit Localization</li><li>Edit Phone Number</li></ol></html>", "Choose a option", 2);
-                switch (familyEditOption) {
-                    //edit family's last name
-                    case "1" -> {
-                        try {
-                            editFamilyLastname();
-                            break;
-                        } catch (IOException ex) {
-                            Logger.getLogger(FamilyManager.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    }
-                    //edit family's location data
-                    case "2" ->
-                        new UpdateLocalization().setVisible(true);
-
-                    //edit family's phone number
-                    case "3" -> {
-                        try {
-                            editPhonenumber();
-                            break;
-                        } catch (IOException ex) {
-                            Logger.getLogger(FamilyManager.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    }
-
-                    default -> {
-                        JOptionPane.showMessageDialog(null, "Choose a valid option !", "Option not found", 2);
-                    }
-
-                }
-                break;
-
-            case "2":
-                //edit father data
-                String fatherEditOption = JOptionPane.showInputDialog(null, "<html>Wich father data would you like to edit ?<br><ol><li>Edit Name</li></ol></html>", "Choose a option", 2);
-                switch (fatherEditOption) {
-                    case "1" -> {
-                        try {
-                            editFatherName();
-                        } catch (IOException ex) {
-                            Logger.getLogger(FamilyManager.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    }
-                    default ->
-                        JOptionPane.showMessageDialog(null, "Present a valid option !", "Option not found", 2);
-                }
-                break;
-
-            case "3":
-                //edit father data
-                String motherEditOption = JOptionPane.showInputDialog(null, "<html>Wich mother data would you like to edit ?<br><ol><li>Edit Name</li></ol></html>", "Choose a option", 2);
-                switch (motherEditOption) {
-                    case "1" -> {
-                        try {
-                            editMotherName();
-                        } catch (IOException ex) {
-                            Logger.getLogger(FamilyManager.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    }
-                    default ->
-                        JOptionPane.showMessageDialog(null, "Present a valid option !", "Option not found", 2);
-                }
-                break;
-            case "4":
-                break;
-            default:
-                JOptionPane.showMessageDialog(null, "Present a valid option !", "Option not found", 2);
-                break;
-        }
-    }//GEN-LAST:event_jEditDataPanelMouseClicked
-
     private void goToLoginTextMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_goToLoginTextMouseClicked
         // goes to login screen
         dispose();
@@ -631,16 +452,10 @@ public class FamilyManager extends javax.swing.JFrame {
     private javax.swing.JLabel jAdminTitle;
     private javax.swing.JPanel jBackground;
     private javax.swing.JLabel jCompanyIcon;
-    private javax.swing.JPanel jEditDataPanel;
     private javax.swing.JLabel jRegisterCompanyTitle1;
-    private javax.swing.JLabel jRegisterFamilyTitle;
-    private javax.swing.JLabel jRegisterFamilyTitle1;
-    private javax.swing.JPanel jRegistratePanel;
     private javax.swing.JPanel jRegistratePanel1;
     private javax.swing.JPanel jRegistratePanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel jSystemLogo;
-    private javax.swing.JLabel jfamilyIcon;
-    private javax.swing.JLabel jfamilyIcon1;
     // End of variables declaration//GEN-END:variables
 }
